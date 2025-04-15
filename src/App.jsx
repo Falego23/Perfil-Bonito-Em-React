93d0f9e4e5884c7fd27c490aff354ba5b76242f6
Falego23 <falegofrijos@gmail.com>
2025-04-15 03:43:42 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App(){
+
+  return (
+    <div>
+      <form>
+      <img src="/images/45d348e90f4d6ed79a66d76f6218672f.jpg"/>
+      <h1>FALEGO</h1>
+      <h3>Brazil, Rio de Janeiro</h3>
+      <p>Fullstack Developer</p>
+      <span>3 Anos de Experiência</span>
+      <a class="tag_a312312" href="https://github.com/Falego23">Github</a>
+      <a class="tag_a312312" href="https://open.spotify.com/playlist/5hE1wjcQ8Y0zaM5PaR8kTV">Spotify</a>
+      </form>
+      </div>
+  );
+}
+
+export default App;
